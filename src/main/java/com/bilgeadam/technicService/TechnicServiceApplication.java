3018101ff1e18829cddb7e91843f242d055bdf2f
canduman00 <119858014+canduman00@users.noreply.github.com>
2023-12-27 15:41:40 +0300

    project init

---
+++ src/main/java/com/bilgeadam/technicService/TechnicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.technicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicServiceApplication.class, args);
+	}
+
+}
